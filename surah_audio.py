582ccb593ba73904ed5b40d05c7080e3a08834fb
1 <alshehriazo@gmail.com>
2025-06-18 19:05:57 +0300

    Update surah_audio.py

--- surah_audio.py
+++ surah_audio.py
@@ -1,15 +1,14 @@
+
 # روابط لتلاوة كاملة بصوت مشاري العفاسي (مباشرة وقابلة للتنزيل)
 surahs = [
-    {
-        "name": "الفاتحة",
-        "audio": "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/001.mp3"
-    },
-    {
-        "name": "البقرة",
-        "audio": "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/002.mp3"
-    },
-    {
-        "name": "الناس",
-        "audio": "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/114.mp3"
-    }
+    {"name": "الفاتحة", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/001.mp3"},
+    {"name": "البقرة", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/002.mp3"},
+    {"name": "آل عمران", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/003.mp3"},
+    {"name": "النساء", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/004.mp3"},
+    {"name": "المائدة", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/005.mp3"},
+    {"name": "الأنعام", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/006.mp3"},
+    {"name": "الأعراف", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/007.mp3"},
+    {"name": "الأنفال", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/008.mp3"},
+    {"name": "التوبة", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/009.mp3"},
+    {"name": "يونس", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/010.mp3"}
 ]
