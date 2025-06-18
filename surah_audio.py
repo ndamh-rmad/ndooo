f2526b0632ac181cf5ac06d004726ce5ac4ea5b6
1 <alshehriazo@gmail.com>
2025-06-18 07:11:13 +0300

    Create surah_audio.py

---
+++ surah_audio.py
@@ -0,0 +1,49 @@
+# قائمة السور مع روابط التسجيلات لهيثم الدخين
+surahs = [
+    {
+        "name": "الفاتحة",
+        "audio": "https://server.mp3quran.net/huth/001.mp3"
+    },
+    {
+        "name": "البقرة",
+        "audio": "https://server.mp3quran.net/huth/002.mp3"
+    },
+    {
+        "name": "آل عمران",
+        "audio": "https://server.mp3quran.net/huth/003.mp3"
+    },
+    {
+        "name": "النساء",
+        "audio": "https://server.mp3quran.net/huth/004.mp3"
+    },
+    {
+        "name": "المائدة",
+        "audio": "https://server.mp3quran.net/huth/005.mp3"
+    },
+    {
+        "name": "الأنعام",
+        "audio": "https://server.mp3quran.net/huth/006.mp3"
+    },
+    {
+        "name": "الأعراف",
+        "audio": "https://server.mp3quran.net/huth/007.mp3"
+    },
+    {
+        "name": "الأنفال",
+        "audio": "https://server.mp3quran.net/huth/008.mp3"
+    },
+    {
+        "name": "التوبة",
+        "audio": "https://server.mp3quran.net/huth/009.mp3"
+    },
+    {
+        "name": "يونس",
+        "audio": "https://server.mp3quran.net/huth/010.mp3"
+    },
+    # يمكنك إضافة المزيد من السور هنا
+    # ...
+    {
+        "name": "الناس",
+        "audio": "https://server.mp3quran.net/huth/114.mp3"
+    }
+]
